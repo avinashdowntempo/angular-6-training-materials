709afcfe7fcfe2d39c20e3411c1d306a4ec57868
avinashdowntempo <avinashj4394@gmail.com>
2018-10-24 10:24:35 +0530

    commit 2

--- Day 1/survey-app/src/app/app.component.ts
+++ Day 1/survey-app/src/app/app.component.ts
@@ -10,4 +10,28 @@ export class AppComponent {
   questions = [{ question: 'Was the Training UseFul?', type: 'mandatory' },
   { question: 'How Efficient is the training?', type: 'optional' },
   { question: 'Was the trainer able to cover most concepts?', type: 'mandatory' }];
+  displayMessage(question) {
+    console.table(this.questions);
+    // tslint:disable-next-line:no-console
+    console.time('test timer');
+    setTimeout(() => {
+      // tslint:disable-next-line:no-console
+      console.log(question);
+      console.warn(question);
+      console.error(question);
+      // tslint:disable-next-line:no-console
+      console.info(question);
+      console.log('%c ' + question, 'background: #222; color: #bada55');
+      // tslint:disable-next-line:no-console
+      console.timeEnd('test timer');
+      this.traceFunction();
+    }, 2000);
+    // debugger;
+    // alert('the selected question is \n' + question);
+  }
+  traceFunction() {
+    console.log('im being traced');
+    // tslint:disable-next-line:no-console
+    // console.trace('trace me');
+  }
 }
